783f59c40fe91754a8e1f40fdbf374b59cd1a344
mfdali <monikerj@ymail.com>
2024-03-01 18:33:21 -0300

    Update sql tables

--- data/alunos.sql
+++ data/alunos.sql
@@ -4,7 +4,7 @@ CREATE TABLE alunos (
     id integer NOT NULL,
     nome character(30) NOT NULL,
     idade integer NOT NULL,
-    curso character(20),
+    curso character(40)
 );
 
 INSERT INTO alunos (id, nome, idade, curso)
@@ -12,5 +12,25 @@ VALUES
 (2536, 'ana silva', 17, 'biologia'),
 (0123, 'joao nascimento', 25, 'educaçao física'),
 (1799, 'hugo leal', 31, 'administração'),
+(5432, 'Pedro Oliveira', 20, 'Psicologia'),
+(2647, 'Juliana Lima', 22, 'Medicina'),
+(3798, 'Lucas Fernandes', 23, 'Engenharia Civil'),
+(1854, 'Carolina Souza', 24, 'Arquitetura'),
+(6721, 'Gabriel Costa', 18, 'Direito'),
+(8912, 'Amanda Santos', 26, 'Administração'),
+(4268, 'Matheus Pereira', 20, 'Ciência da Computação'),
+(1397, 'Camila Oliveira', 31, 'Enfermagem'),
+(5089, 'Rodrigo Silva', 22, 'Biologia'),
+(7523, 'Beatriz Ferreira', 19, 'Educação Física'),
+(6185, 'Fernanda Almeida', 23, 'Economia'),
+(3579, 'Rafael Martins', 20, 'Farmácia'),
+(9274, 'Larissa Barbosa', 25, 'Administração'),
+(6032, 'Diego Sousa', 26, 'Engenharia Elétrica'),
+(2187, 'Mariana Costa', 27, 'Comunicação Social'),
+(8351, 'Paulo Oliveira', 22, 'Medicina Veterinária'),
+(4096, 'Patrícia Santos', 20, 'Nutrição'),
+(5762, 'Thiago Silva', 23, 'Física'),
+(2948, 'Aline Ferreira', 34, 'Psicologia'),
+(7105, 'Vinícius Souza', 19, 'Direito'),
 (8016, 'maria magalhaes', 19, 'economia'),
 (8911, 'ana morello', 21, 'engenharia');
--- data/clientes.sql
+++ data/clientes.sql
@@ -1,3 +1,5 @@
+SET SCHEMA 'clientes';
+
 CREATE TABLE clientes (
     id SERIAL PRIMARY KEY,
     nome character(30) NOT NULL,
@@ -11,5 +13,20 @@ VALUES
 ('Joana Donato', 35, 2506.50),
 ('Maria Nascimento', 28, 97.75),
 ('Andre Jacaranda', 40, 950.25),
+('Ana Silva', 25, 1500.00),
+('Lucas Oliveira', 32, 750.25),
+('Camila Santos', 45, 3000.50),
+('Pedro Costa', 22, 450.75),
+('Juliana Ferreira', 38, 20000.00),
+('Marcos Lima', 29, 1250.30),
+('Carla Souza', 41, 850.99),
+('Rafael Almeida', 27, 560.25),
+('Aline Pereira', 36, 18000.75),
+('Fernando Vieira', 31, 3750.50),
+('Isabela Ribeiro', 26, 850.25),
+('Thiago Oliveira', 39, 22000.00),
+('Paula Martins', 23, 950.30),
+('Roberto Santos', 37, 4250.99),
+('Mariana Costa', 30, 1100.25),
 ('Emily Alves', 33, 10350.20),
 ('Mario Porto', 29, 30050.86);
